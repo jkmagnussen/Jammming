acbf0b063b50b00e1304be3e256b6943805436b0
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-05-20 01:58:06 +0100

    added api previews

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Track.css";
 
+import PlayPause from "./Media/playPause.png";
+
 class Track extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      playing: false,
+      audio: new Audio(),
+    };
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.playSong = this.playSong.bind(this);
+    this.pauseSong = this.pauseSong.bind(this);
   }
 
   renderAction() {
@@ -33,15 +41,33 @@ class Track extends React.Component {
     this.props.onRemove(this.props.track);
   }
 
+  playSong() {
+    console.log("play");
+    this.setState({ playing: true });
+    this.state.audio.src = this.props.track.preview;
+    this.state.audio.play();
+  }
+
+  pauseSong() {
+    console.log("pause");
+    this.setState({ playing: false });
+    this.state.audio.pause();
+  }
+
   render() {
     return (
       <div className="Track">
         <div className="Track-information">
           <h3>{this.props.track.name}</h3>
           <p>
-            {this.props.track.artist} | {this.props.track.album}
+            {this.props.track.artist} | {this.props.track.album} |{" "}
           </p>
         </div>
+        <img
+          className="playpause"
+          src={PlayPause}
+          onClick={this.state.playing ? this.pauseSong : this.playSong}
+        />
         <button class="Track-action">{this.renderAction()}</button>
       </div>
     );
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -47,6 +47,7 @@ const Spotify = {
             artist: track.artists[0].name,
             album: track.album.name,
             uri: track.uri,
+            preview: track.preview_url,
           }));
         }
       });
